230e78fed66983753edc821c9af0c692fc9acc06
Gustavo Ribeiro <gugzribeiro@gmail.com>
2024-11-02 22:03:32 -0300

    Projeto Concluído!

--- src/index.php
+++ src/index.php
@@ -3,23 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário</title>
-<body>
-    <h1>Formulário de Preenchimento</h1>
-    <form action="process.php" method="post">
-        <label for="campo1">Campo 1:</label>
-        <input type="text" id="campo1" name="campo1" required><br><br>
-        
-        <label for="campo2">Campo 2:</label>
-        <input type="text" id="campo2" name="campo2" required><br><br>
-        
-        <label for="campo3">Campo 3:</label>
-        <input type="text" id="campo3" name="campo3" required><br><br>
-        
-        <button type="button" onclick="loadDoc()">Enviar</button>
-
-        <script src="./views/JavaScript/SistemaAjax.js"></script>
+    <title>Aula de Ajax</title>
+    <script>
+        function loadContent() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', './controllers/testeController.php', true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    document.getElementById('teste').innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
+        }
+    </script>
 </head>
-    </form>
+<body>
+    <h1>Aula JS com Ajax</h1>
+    <button onclick="loadContent()">Faça um Teste</button>
+    <div id="teste"></div>
 </body>
 </html>
