e3717418df282b054ebd6cf6d9586c9165879ff9
Gustavo Ribeiro <gugzribeiro@gmail.com>
2024-11-02 22:10:08 -0300

    Mudanças realizadas utilizando console.log

--- src/index.php
+++ src/index.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula de Ajax</title>
     <script>
+
+        // Professor, kkkk, se você ver os commits dessa atividade vai ver quanto tempo fiquei quebrando a cabeça e estudando para tentar fazer um código bem feito...
+        
+        console.log("Iniciando Atividade...");
+
         function loadContent() {
             var xhr = new XMLHttpRequest();
             xhr.open('GET', './controllers/testeController.php', true);
@@ -14,6 +19,7 @@
                 }
             };
             xhr.send();
+            console.log("Finalizando Atividade...");
         }
     </script>
 </head>
